1957576614a689589c438327ff239e1d3425808d
Василий Лупуляк <v_lupuleac@mail.ru>
2020-11-01 23:50:02 +0300

    Add camera tracking implementation

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -4,12 +4,10 @@ __all__ = [
     'track_and_calc_colors'
 ]
 
+import numpy as np
+import cv2
 from typing import List, Optional, Tuple
 
-import numpy as np
-
-from corners import CornerStorage
-from data3d import CameraParameters, PointCloud, Pose
 import frameseq
 from _camtrack import (
     PointCloudBuilder,
@@ -17,8 +15,107 @@ from _camtrack import (
     calc_point_cloud_colors,
     pose_to_view_mat3x4,
     to_opencv_camera_mat3x3,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    build_correspondences,
+    triangulate_correspondences,
+    TriangulationParameters,
+    rodrigues_and_translation_to_view_mat3x4
 )
+from corners import CornerStorage
+from data3d import CameraParameters, PointCloud, Pose
+
+
+class CameraTracker:
+
+    MAX_REPR_ERROR = 3.0
+
+    def __init__(self,
+                 intrinsic_mat,
+                 corners: CornerStorage,
+                 known_view_1: Optional[Tuple[int, Pose]] = None,
+                 known_view_2: Optional[Tuple[int, Pose]] = None):
+        if known_view_1 is None or known_view_2 is None:
+            raise NotImplementedError()
+        self.intrinsic_mat = intrinsic_mat
+        self.corners = corners
+        self.frame_count = len(corners)
+        self.pc_builder = PointCloudBuilder()
+        frame_1 = known_view_1[0]
+        frame_2 = known_view_2[0]
+        self.is_known = np.full(self.frame_count, False)
+        self.is_known[frame_1] = True
+        self.is_known[frame_2] = True
+        self.view_mats = [pose_to_view_mat3x4(known_view_1[1])] * self.frame_count
+        self.view_mats[frame_2] = pose_to_view_mat3x4(known_view_2[1])
+        self._extend_point_cloud(frame_1, frame_2)
+
+    def _extend_point_cloud(self, frame_1, frame_2, max_reprojection_error=MAX_REPR_ERROR, min_angle=2.0):
+        corners_1 = self.corners[frame_1]
+        corners_2 = self.corners[frame_2]
+        correspondences = build_correspondences(corners_1, corners_2, self.pc_builder.ids)
+        triangulation_parameters = TriangulationParameters(max_reprojection_error,
+                                                           min_angle,
+                                                           0)
+        points3d, ids, _ = triangulate_correspondences(correspondences,
+                                                       self.view_mats[frame_1],
+                                                       self.view_mats[frame_2],
+                                                       self.intrinsic_mat,
+                                                       triangulation_parameters)
+        self.pc_builder.add_points(ids, points3d)
+
+    def _get_pose(self, frame):
+        corners = self.corners[frame]
+        points3d = self.pc_builder.points
+        _, corners_ids, points_ids = np.intersect1d(corners.ids,
+                                                    self.pc_builder.ids,
+                                                    assume_unique=True,
+                                                    return_indices=True)
+        points3d = points3d[points_ids]
+        corners_points = corners.points[corners_ids]
+        if len(points3d) < 4:
+            return
+
+        success, R, t, inliers = cv2.solvePnPRansac(objectPoints=points3d,
+                                                    imagePoints=corners_points,
+                                                    cameraMatrix=self.intrinsic_mat,
+                                                    reprojectionError=self.MAX_REPR_ERROR,
+                                                    confidence=0.999,
+                                                    distCoeffs=None,
+                                                    flags=cv2.SOLVEPNP_EPNP)
+
+        points3d = points3d[inliers]
+        corners_points = corners_points[inliers]
+
+        success, R, t = cv2.solvePnP(objectPoints=points3d,
+                                     imagePoints=corners_points,
+                                     cameraMatrix=self.intrinsic_mat,
+                                     distCoeffs=None,
+                                     useExtrinsicGuess=True,
+                                     rvec=R,
+                                     tvec=t,
+                                     flags=cv2.SOLVEPNP_ITERATIVE)
+
+        self.view_mats[frame] = rodrigues_and_translation_to_view_mat3x4(R, t)
+        return R, t
+
+    def track(self):
+        while not self.is_known.all():
+            unknown_frames = np.arange(self.frame_count)[self.is_known == False]
+            update_results = [(frame, self._get_pose(frame)) for frame in unknown_frames]
+            updated = [frame for frame, res in update_results if res is not None]
+            if len(updated) == 0:
+                break
+            self.is_known[updated] = True
+
+            old_frames = np.arange(self.frame_count)[self.is_known]
+            for frame_1 in updated:
+                for old_frame in old_frames:
+                    self._extend_point_cloud(frame_1, old_frame)
+                for new_frame in updated:
+                    if new_frame < frame_1:
+                        self._extend_point_cloud(frame_1, new_frame)
+
+            return self.view_mats, self.pc_builder
 
 
 def track_and_calc_colors(camera_parameters: CameraParameters,
@@ -27,8 +124,6 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
 
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
     intrinsic_mat = to_opencv_camera_mat3x3(
@@ -36,12 +131,10 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         rgb_sequence[0].shape[0]
     )
 
-    # TODO: implement
-    frame_count = len(corner_storage)
-    view_mats = [pose_to_view_mat3x4(known_view_1[1])] * frame_count
-    corners_0 = corner_storage[0]
-    point_cloud_builder = PointCloudBuilder(corners_0.ids[:1],
-                                            np.zeros((1, 3)))
+    view_mats, point_cloud_builder = CameraTracker(intrinsic_mat,
+                                                   corner_storage,
+                                                   known_view_1,
+                                                   known_view_2).track()
 
     calc_point_cloud_colors(
         point_cloud_builder,
@@ -51,6 +144,7 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         corner_storage,
         5.0
     )
+
     point_cloud = point_cloud_builder.build_point_cloud()
     poses = list(map(view_mat3x4_to_pose, view_mats))
     return poses, point_cloud
